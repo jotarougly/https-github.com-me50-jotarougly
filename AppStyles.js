e32d2ecbc11ff1503e4a4ec3f56497183435c229
GOMADO Ayao Mawuli Stéphane <stemagomado@gmail.com>
2019-12-14 23:44:19 +0000

    Project2 corrections upload

--- AppStyles.js
+++ AppStyles.js
@@ -1,25 +1,6 @@
 import { StyleSheet } from 'react-native';
-import { getOrientationAsync } from 'expo/build/ScreenOrientation/ScreenOrientation';
 
 export const appStyles = StyleSheet.create({
-    mainContainer:{
-        flex: 1
-    },
-    appWording:{
-        marginTop:30,
-        fontSize: 20,
-        padding: 10,
-        textAlign: 'center',
-        color: 'cyan',
-    },
-    homeText:{
-        marginTop:10,
-        fontSize: 15,
-        padding: 10,
-        textAlign: 'center',
-        color: 'blue',
-
-    },
     textInput:{
         margin:10,
         height: 30,
@@ -30,10 +11,6 @@ export const appStyles = StyleSheet.create({
         fontSize: 15,
         padding: 5,
         textAlign: 'justify',
-    },
-    sectionStyle:{
-        flex:1,
-         flexDirection:'row'
     },
     sectionTitleStyle:{
         backgroundColor:'grey'
